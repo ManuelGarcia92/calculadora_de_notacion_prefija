a5af05b7b94b1eb8ed14e26e039647a19d28914d
ManuelGarcia92 <jmgb7738@gmail.com>
2026-09-23 16:49:19 -0400

    Pequeña correción

--- evaluador.py
+++ evaluador.py
@@ -2,12 +2,11 @@ class Evaluador:
     def __init__(self, instrucciones):
         self.instrucciones = instrucciones
 
-    def evaluar(self):
+    def evaluar(self) -> int | float | None:
         resultado = None
         if self.instrucciones is not None:
             for instruccion in self.instrucciones:
                 resultado = instruccion.evaluar()
-            return resultado
-        return None
-
+        return resultado
+        
     
